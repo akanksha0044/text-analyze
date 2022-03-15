68433253f2a8c3a7427460149157c24876810ff9
[akanksha004] <[akankshamaurya0044@gmail.com]>
2022-03-15 22:13:45 +0530

    message

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Navbar from './Component/Navbar';
+import TextForm from './Component/TextForm';
+import { useState } from 'react/cjs/react.development';
+
+
 
 function App() {
+  const [mode, setMode] = useState('dark');
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = 'grey';
+    }
+    else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar mode={mode} toggleMode={toggleMode} />
+      <div className="container my-3">
+
+        <TextForm heading="Enter the text to Analyze" />
+
+      </div>
+
+    </>
   );
 }
 
